e9c1772368ed40f5eca43935617b34cd9ade7d08
Surya <jskandepi@gmail.com>
2020-06-04 20:29:57 +0530

    3

--- app/src/main/java/com/example/mobileproj/MainActivity.java
+++ app/src/main/java/com/example/mobileproj/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.mobileproj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.homemenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item2:
+                Toast.makeText(this,"1", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this,"2",Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return true;
+        }
+
     }
 }
