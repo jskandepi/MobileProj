01271f902d89684fe4d4f88597b28841f4519e88
Surya <jskandepi@gmail.com>
2020-06-04 22:50:38 +0530

    5

--- app/src/main/java/com/example/mobileproj/MainActivity.java
+++ app/src/main/java/com/example/mobileproj/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Snackbar.make(view,"Replace with your own action",Snackbar.LENGTH_LONG).setAction("Action",null).show();}
+        });
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            Intent intent = new Intent(this,homepage.class);
+            startActivity(intent);
+            finish();
+        }
     }
 
     @Override
--- app/src/main/java/com/example/mobileproj/homepage.java
+++ app/src/main/java/com/example/mobileproj/homepage.java
@@ -1,14 +1,75 @@
 package com.example.mobileproj;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class homepage extends AppCompatActivity {
+
+    private static final String TAG = "homepage";
+    int AUTHUI_REQUESTCODE = 10001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() != null){
+            startActivity(new Intent(this,MainActivity.class));
+            this.finish();
+        }
+    }
+
+    public void logreg(View view){
+
+        List<AuthUI.IdpConfig> provider = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build()
+        );
+
+        Intent intent = AuthUI.getInstance().
+                createSignInIntentBuilder().
+                setAvailableProviders(provider).
+                setLogo(R.drawable.download).
+                build();
+
+        startActivityForResult(intent, AUTHUI_REQUESTCODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AUTHUI_REQUESTCODE){
+            if(resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d(TAG,"onActivityResult: "+user.getEmail());
+                if(user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()){
+                    Toast.makeText(this,"Welcome new User",Toast.LENGTH_SHORT).show();
+                }
+                else Toast.makeText(this, "Welcome Back",Toast.LENGTH_SHORT).show();
+
+                startActivity(new Intent(this,MainActivity.class));
+                this.finish();
+            }
+            else{
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if(response == null){
+                    Log.d(TAG,"User has cancelled Sign-in request");
+                }
+                else Log.e(TAG,"Error:",response.getError());
+            }
+        }
     }
 }
